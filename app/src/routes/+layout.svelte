628e1c8de36afab006b9e18d14b832daf9c31181
proud-p <avika.pulges@hogarth.com>
2024-11-07 15:39:42 +0000

    updating placeholders

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,4 +3,13 @@
 	let { children } = $props();
 </script>
 
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/resources">Resources</a>
+	<a href="/events">Events</a>
+	<a href="/donate">Donate</a>
+	<a href="/faq">FAQ</a>
+  </nav>
+
 {@render children()}
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,1 +1,38 @@
-<h1>Live Code London</h1>
+
+<div class="container">
+    <div class="title-banner">
+    <h1>Live Code London</h1>
+    <section class="next-event">
+        <h2>Next Upcoming Event</h2>
+        <p>Placeholder for next upcoming event</p>
+    </section>
+    </div>
+
+    <div class="menu-items">
+    <!-- Content for menu items can be added here -->
+    <p>Place holder for menu items</p>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        height: 100vh;
+        width: 100vw;
+    }
+  .title-banner {
+    padding: 20px;
+    background-color: #f050f0;
+    text-align: center;
+    width: 50%;
+  }
+  .next-event {
+    margin-top: 10px;
+    background-color: aliceblue;
+  }
+  .menu-items {
+    padding: 20px;
+    background-color: #e0e060;
+    width: 50%;
+  }
+</style>
